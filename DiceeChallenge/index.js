d46dbcc940f1b26329f68386fbc6aa036b2ed061
Matsubon <103348300+Matsubon@users.noreply.github.com>
2023-04-19 07:50:23 +0900

    index.js を更新

--- DiceeChallenge/index.js
+++ DiceeChallenge/index.js
@@ -15,6 +15,6 @@ if (randomNumber1 > randomNumber2){
 } else if(randomNumber2 > randomNumber1) {
     document.querySelector("h1").innerHTML = "player2 wins!"
 } else{
-    document.querySelector("h1").innerHTML = "even"
+    document.querySelector("h1").innerHTML = "draw!"
 }
 
