675175f0343f141b2e121af2da132bd4630da8fb
Matsubon <103348300+Matsubon@users.noreply.github.com>
2023-04-25 07:14:23 +0900

    index.js を更新

--- DiceeChallenge/index.js
+++ DiceeChallenge/index.js
@@ -5,8 +5,8 @@ var image1 = document.querySelectorAll("img").[0];
 image1.setAttribute("src",randomImageSource1);
 
 var randomNumber2 = Math.floor(Math.random()*6) +1;
-var randomDiceImage2 = "dice" + randomNumber1 + ".png";
-var randomImageSource2 = "images/" + randomDiceImage1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
 var image2 = document.querySelectorAll("img").[1];
 image2.setAttribute("src",randomImageSource2);
 
