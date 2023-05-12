3100eea55970b777dad9212256f0c4f97d030515
Matsubon <103348300+Matsubon@users.noreply.github.com>
2023-05-12 17:58:01 +0900

    index.js を更新

--- DOM/drumkit/index.js
+++ DOM/drumkit/index.js
@@ -1,7 +1,7 @@
 var numberofDrumButton = document.querySelectorAll(".drum").length;
 
 for(var i = 0 i<numberofDrumButton; i++){
-document.querySelectorall(".drum")[i].addEventListenner(click, handleClick())
+document.querySelectorall(".drum")[i].addEventListenner(click, handleClick());
 
 Function handleClick{
  var buttonInnerHTML= this.innertext;
