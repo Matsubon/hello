28416b3b2d2d0dc1ce0422fb78437723ef02086b
Matsubon <103348300+Matsubon@users.noreply.github.com>
2023-05-18 07:24:27 +0900

    Update index.js

--- DOM/drumkit/index.js
+++ DOM/drumkit/index.js
@@ -1,44 +1,39 @@
-var numberofDrumButton = document.querySelectorAll(".drum").length;
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
 
-for(var i = 0 i<numberofDrumButton; i++){
-document.querySelectorall(".drum")[i].addEventListener(click, handleClick(){
-
-function handleClick{
- var buttonInnerHTML= this.innertext;
-switch (buttonInnerHTML) {
-  case "w":
-    var tom1 = new Audio("sounds/tom-1.mp3");
-     tom1.play();
-    break;
-  case "a":
-    var tom2 = new Audio("sounds/tom-2.mp3");
-     tom2.play();
-    break;
-  case "s":
-    var tom3 = new Audio("sounds/tom-3.mp3");
-     tom3.play();
-    break;
-  case "d":
-    var tom4 = new Audio("sounds/tom-4.mp3");
-     tom4.play();
-    break;
-  case "j":
-    var snare = new Audio("sounds/snare.mp3");
-     snare.play();
-    break;
-  case "k":
-    var crash = new Audio("sounds/crash.mp3");
-     crash.play();
-    break;
-  case "l":
-    var kick = new Audio("sounds/kick.mp3");
-     kick.play();
-    break;
- default:
-  console.log(buttonInnerHTML);
+for (var i = 0; i < numberOfDrumButton; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function handleClick() {
+    var buttonInnerHTML = this.innerText;
+    switch (buttonInnerHTML) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      case "l":
+        var kick = new Audio("sounds/kick.mp3");
+        kick.play();
+        break;
+      default:
+        console.log(buttonInnerHTML);
+    }
+  });
 }
-
-});
-
-}
-    
